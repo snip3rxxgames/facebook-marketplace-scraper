d39c9219a2c7f3d7c11bafa8a7ac96c66fd5a194
snip3rxxgames <snip3rgameshelp@gmail.com>
2025-05-08 21:08:18 -0400

    Update gui.py

--- gui.py
+++ gui.py
@@ -4,7 +4,7 @@ import requests
 from PIL import Image
 
 # Create a title for the web app.
-st.title("Passivebot's Facebook Marketplace Scraper")
+st.title(snip3r's marketplace")
 
 # Add a list of supported cities.
 supported_cities = ["New York", "Los Angeles", "Las Vegas", "Chicago", "Houston", "San Antonio", "Miami", "Orlando", "San Diego", "Arlington", "Baltimore", "Cincinnati", "Denver", "Fort Worth", "Jacksonville", "Memphis", "Nashville", "Philadelphia", "Portland", "San Jose", "Tucson", "Atlanta", "Boston", "Columbus", "Detroit", "Honolulu", "Kansas City", "New Orleans", "Phoenix", "Seattle", "Washington DC", "Milwaukee", "Sacramento", "Austin", "Charlotte", "Dallas", "El Paso", "Indianapolis", "Louisville", "Minneapolis", "Oklahoma City", "Pittsburgh", "San Francisco", "Tampa"]
